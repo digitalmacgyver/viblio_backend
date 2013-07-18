37086f85497233a5143c4a73517214f85b02ce01
Matt Hayward <matt@viblio.com>
2013-07-18 23:07:59 +0000

    Initial version installed

--- schema/examples/example.py
+++ schema/examples/example.py
@@ -5,7 +5,7 @@ from sqlalchemy import *
 
 # Set up connection information.
 
-verbose = True
+verbose = False
 
 username = 'video_dev'
 password = 'video_dev'
@@ -63,27 +63,29 @@ print "Inserted video id was:", result.inserted_primary_key
 # Note 1: all rows must have the same columns.
 # Note 2: Any columns not defined will recieve default values.
 print "Inserting multiple rows into the video table."
-result = conn.execute( video.insert()
-                       .returning( [video.c.id, video.c.lat, video.c.created] ), 
+result = conn.execute( video.insert(),
                        [
-        { 'title' : 'unit_test_insert', 'lat' : 220.4, 'lng' : 2.123 },
-        { 'title' : 'unit_test_insert', 'lat' : 110.2, 'lng' : 321.132 },
+        { 'description' : 'unit_test_insert', 'lat' : 220.4, 'lng' : 2.123 },
+        { 'description' : 'unit_test_insert', 'lat' : 110.2, 'lng' : 321.132 },
         # Note, we can have as many rows as we want here.
         ] )
-print "Inserted id, lat, and created were"
-print result.fetchall()
+# Further note - unlike the one by one rows above you can't get the
+# IDs of rows inserted in this way.
+
+print "Updating a row in the video table."
+result = conn.execute( video.update().where( video.c.lat == 220.4 ).values( lat=330.6 ) );
 
 # Add a video_encoding for each video.  Note that we must provide a
 # video_id that matches an Id found in the video table when adding
 # these rows.
 #
 # Begin by getting a list of videos in the video table.
-for videoId in self.conn.execute( 
-    select( [self.video.c.id] )
-    .where( self.video.c.description == 'unit_test_insert' ) ):
+for videoId in conn.execute( 
+    select( [video.c.id] )
+    .where( video.c.description == 'unit_test_insert' ) ):
     # Then for each video, add a child row in the video_encoding_insert table.
-    print "Adding a video_encoding row associated with video id:", result[0]
-    result = self.conn.execute( self.video_encoding.insert(),
+    print "Adding a video_encoding row associated with video id:", videoId[0]
+    result = conn.execute( video_encoding.insert(),
                                 id=None,
                                 video_id=videoId[0],
                                 url="http://s3/video-name.mp4",
@@ -94,19 +96,19 @@ for videoId in self.conn.execute(
                                 lenth=2202.23,
                                 width=720,
                                 height=480 );
-    print "Inserted video_encoding had id:", result[0]
+    print "Inserted video_encoding had id:", result.inserted_primary_key[0]
     result.close()
 
         
-for encodingId, videoId in self.conn.execute( 
-    select( [self.video_encoding.c.id, self.video_encoding.c.video_id ] )
-    .where( self.video_encoding.c.format == 'unit_test_insert' ) ):
-    print "Adding image for video encoding:", encodingId[0]
+for ids in conn.execute( 
+    select( [video_encoding.c.id, video_encoding.c.video_id ] )
+    .where( video_encoding.c.format == 'unit_test_insert' ) ):
+    print "Adding image for video encoding:", ids[0]
 
-    self.conn.execute( self.image.insert(),
+    conn.execute( image.insert(),
                        id=None,
-                       video_encoding_id=encodingId[0],
-                       video_id=videoId[0],
+                       video_encoding_id=ids[0],
+                       video_id=ids[1],
                        time_stamp=33.004999,
                        url='http://s3/video/0000123.jpg',
                        metadata_url='http://s3/video/0000123.json',
@@ -116,19 +118,19 @@ for encodingId, videoId in self.conn.execute(
 
 # Clean up our tests.
 print "Deleting rows from image."
-self.conn.execute( 
-    self.image.delete()
-    .where( self.image.c.format == 'unit_test_insert' ) )
+conn.execute( 
+    image.delete()
+    .where( image.c.format == 'unit_test_insert' ) )
 
 print "Deleting rows from video_encoding."
-self.conn.execute( 
-    self.video_encoding.delete()
-    .where( self.video_encoding.c.format == 'unit_test_insert' ) )
+conn.execute( 
+    video_encoding.delete()
+    .where( video_encoding.c.format == 'unit_test_insert' ) )
 
 print "Deleting rows from video."
-self.conn.execute( 
-    self.video.delete()
-    .where( self.video.c.description == 'unit_test_insert' ) )
+conn.execute( 
+    video.delete()
+    .where( video.c.description == 'unit_test_insert' ) )
 
 # Close our connections.
 print "Closing database connection."
