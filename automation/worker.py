37bbd400438fe2ad9e8c7abe3a7d2645645e0fe9
Account for running <viblio@ip-10-0-0-120.us-west-2.compute.internal>
2013-07-22 21:48:56 +0000

    Added writing thumbnails and posters to S3

--- automation/worker.py
+++ automation/worker.py
@@ -10,7 +10,9 @@
 # Licence:  Copyright Video Analytics, Inc.
 #           All rights reserved
 # To do:
-# Fork a new pypthon script for each patched file, data logging, error handling, checking before deleting files
+# Fork a new python script for each patched file, data logging, error handling, checking before deleting files
+# data logging - date, file size, file name + error logs (http://docs.python.org/2/howto/logging.html)
+# error handling
 #
 #------------------------------------------------------------------------------
 
@@ -20,8 +22,6 @@ import boto
 import requests
 import json
 from boto.s3.key import Key
-
-viblio_server_url = 'http://staging.viblio.com/services/mediafile/create?$params'
 
 # Hardcoded parameters - needs to become variables later
 uuid = 'BB374034-AAA0-11E2-9D76-00B897344F04'
@@ -44,19 +44,28 @@ for filename in os.listdir('/mnt/uploaded_files'):
             media_filesize = parsed_contents.get('finalLength')
             s3_directory = filename.strip('.json')
             media_filename = filename.strip('.json') + mime_extension
+            print s3_directory
 # Create thumbnail and poster files
-            os.system ('ffmpeg -ss 10 -i /mnt/uploaded_files/' + s3_directory + '-vframes 1 -f image2 
-              -s 128x128 /mnt/uploaded_files/' + s3_directory + _thumbnail.jpg')
+# command = 'ffmpeg -ss 10 -i /mnt/uploaded_files/' + s3_directory + ' -vframes 1 -f image2 - s 128x128 /mnt/uploaded_files/' + s3_directory + '_thumbnail.jpg'
+            command = 'ffmpeg -v quiet -ss 10 -i /mnt/uploaded_files/' + s3_directory + ' -vframes 1 -f image2 -s 128x128 /mnt/uploaded_files/' + s3_directory + '_thumbnail.jpg'
+            os.system (command)
+            command = 'ffmpeg -v quiet -ss 10 -i /mnt/uploaded_files/' + s3_directory + ' -vframes 1 -f image2 -s 240x320 /mnt/uploaded_files/' + s3_directory + '_poster.jpg'
+            os.system (command)
 # Upload to S3
             s3_filename_with_path = s3_directory + '/' + media_filename
             local_filename_with_path = '/mnt/uploaded_files/' + s3_directory
             bucket_contents.key = s3_filename_with_path
             bucket_contents.set_contents_from_filename(local_filename_with_path)
+            bucket_contents.key = s3_filename_with_path + '_thumbnail.jpg'
+            bucket_contents.set_contents_from_filename(local_filename_with_path + '_thumbnail.jpg')
+            bucket_contents.key = s3_filename_with_path + '_poster.jpg'
+            bucket_contents.set_contents_from_filename(local_filename_with_path + '_poster.jpg')
             print 'uploaded ' + local_filename_with_path + ' to ' + s3_filename_with_path
 # Update Viblio server
-            params = {'filename': media_filename, 'mimetype': 'video/' + mime_extension, 'size': media_filesize, 'location':'us', 'bucket_name': config.bucket_name,
+            params = {'uid': uuid, 'filename': media_filename, 'mimetype': 'video/' + mime_extension, 'size': media_filesize, 'location':'us', 'bucket_name': config.bucket_name,
               'uri': s3_directory + '/' + media_filename}
             r = requests.get(config.viblio_server_url, params=params)
+            print r.text
 # delete files, but check successful transfer before delete
-            os.remove('/mnt/uploaded_files/' + filename)
+#           os.remove('/mnt/uploaded_files/' + filename)
 #           os.remove('/mnt/uploaded_files/' + 
