7b8f1e7d57285db042ccec42748e3151f2f581cb
Account for running <viblio@ip-10-0-0-120.us-west-2.compute.internal>
2013-07-23 01:15:59 +0000

    Changed worker to just copy files to S3 and update cat-server

--- automation/worker.py
+++ automation/worker.py
@@ -1,12 +1,11 @@
-
 #!/usr/bin/env python
 #------------------------------------------------------------------------------
-# Name:     work.py
-# Purpose:  moves completed media files to S3 and creates corresponding database entries
+# Name:     worker.py
+# Purpose:  Copies completed media files to S3 and creates corresponding database entries
 #
-# Author:   Bidyut Parruck   
+# Author:   Bidyut Parruck
 #
-# Created:  July 20th, 2013
+# Created:  July 22th, 2013
 # Licence:  Copyright Video Analytics, Inc.
 #           All rights reserved
 # To do:
@@ -15,57 +14,65 @@
 # error handling
 #
 #------------------------------------------------------------------------------
-
 import config
 import os
+import sys
 import boto
 import requests
 import json
 from boto.s3.key import Key
 
+filename = sys.argv[1]
+
 # Hardcoded parameters - needs to become variables later
 uuid = 'BB374034-AAA0-11E2-9D76-00B897344F04'
 mime_extension = '.mts'
 
-# connect to Viblio S3 account and access the table of contents
-s3 = boto.connect_s3(config.awsAccess, config.awsSecret)
-bucket = s3.get_bucket(config.bucket_name)
-bucket_contents = Key(bucket)
+file_contents = open(config.upload_dir + filename)
+parsed_contents = json.load (file_contents)
+media_filesize = parsed_contents.get('finalLength')
+s3_directory = filename.strip('.json')
+filename_only = filename.strip('.json')
 
-# process one file at a time
-for filename in os.listdir('/mnt/uploaded_files'):
-    if filename.endswith('.json'):
-        file_contents = open('/mnt/uploaded_files/' + filename)
-        parsed_contents = json.load (file_contents)
-# Process only completed files with 'state' = 'patched'
-        if parsed_contents.get('finalLength') == parsed_contents.get('offset'):
-# These should be handled by a forked process - one per completed file
-# Eventually node server may directly call this script after file upload is completed
-            media_filesize = parsed_contents.get('finalLength')
-            s3_directory = filename.strip('.json')
-            media_filename = filename.strip('.json') + mime_extension
-            print s3_directory
 # Create thumbnail and poster files
-# command = 'ffmpeg -ss 10 -i /mnt/uploaded_files/' + s3_directory + ' -vframes 1 -f image2 - s 128x128 /mnt/uploaded_files/' + s3_directory + '_thumbnail.jpg'
-            command = 'ffmpeg -v quiet -ss 10 -i /mnt/uploaded_files/' + s3_directory + ' -vframes 1 -f image2 -s 128x128 /mnt/uploaded_files/' + s3_directory + '_thumbnail.jpg'
-            os.system (command)
-            command = 'ffmpeg -v quiet -ss 10 -i /mnt/uploaded_files/' + s3_directory + ' -vframes 1 -f image2 -s 240x320 /mnt/uploaded_files/' + s3_directory + '_poster.jpg'
-            os.system (command)
+try:
+    command = 'ffmpeg -v quiet -ss 10 -i ' +  config.upload_dir + s3_directory + ' -vframes 1 -f image2 -s 128x128 ' + config.upload_dir + s3_directory + '_thumbnail.jpg'
+    os.system (command)
+except:
+    pass
+try:
+    command = 'ffmpeg -v quiet -ss 10 -i ' +  config.upload_dir + s3_directory + ' -vframes 1 -f image2 -s 240x320 ' + config.upload_dir + s3_directory + '_poster.jpg'
+    os.system (command)
+except:
+    pass
+# connect to Viblio S3 account and access the table of contents
+try: 
+    s3 = boto.connect_s3(config.awsAccess, config.awsSecret)
+    bucket = s3.get_bucket(config.bucket_name)
+    bucket_contents = Key(bucket)
+
 # Upload to S3
-            s3_filename_with_path = s3_directory + '/' + media_filename
-            local_filename_with_path = '/mnt/uploaded_files/' + s3_directory
-            bucket_contents.key = s3_filename_with_path
-            bucket_contents.set_contents_from_filename(local_filename_with_path)
-            bucket_contents.key = s3_filename_with_path + '_thumbnail.jpg'
-            bucket_contents.set_contents_from_filename(local_filename_with_path + '_thumbnail.jpg')
-            bucket_contents.key = s3_filename_with_path + '_poster.jpg'
-            bucket_contents.set_contents_from_filename(local_filename_with_path + '_poster.jpg')
-            print 'uploaded ' + local_filename_with_path + ' to ' + s3_filename_with_path
-# Update Viblio server
-            params = {'uid': uuid, 'filename': media_filename, 'mimetype': 'video/' + mime_extension, 'size': media_filesize, 'location':'us', 'bucket_name': config.bucket_name,
-              'uri': s3_directory + '/' + media_filename}
-            r = requests.get(config.viblio_server_url, params=params)
-            print r.text
+    local_filename_with_path = config.upload_dir + filename_only
+    bucket_contents.key = s3_directory + '/' + filename_only + mime_extension
+    bucket_contents.set_contents_from_filename(local_filename_with_path)
+
+    bucket_contents.key = s3_directory + '/' + filename_only + '_thumbnail.jpg'
+    bucket_contents.set_contents_from_filename(local_filename_with_path + '_thumbnail.jpg')
+
+    bucket_contents.key = s3_directory + '/' + filename_only + '_poster.jpg'
+    bucket_contents.set_contents_from_filename(local_filename_with_path + '_poster.jpg')
+    print 'uploaded ' + local_filename_with_path
+    
 # delete files, but check successful transfer before delete
-#           os.remove('/mnt/uploaded_files/' + filename)
-#           os.remove('/mnt/uploaded_files/' + 
+    os.remove(local_filename_with_path)
+    os.remove(local_filename_with_path + '_thumbnail.jpg')
+    os.remove(local_filename_with_path + '_poster.jpg')
+    
+except:
+    print 'S3 upload unsucessful'
+    pass
+# Update Viblio server
+params = {'uid': uuid, 'filename': filename_only + mime_extension, 'mimetype': 'video/mp4', 'size': media_filesize, 'location':'us', 'bucket_name': config.bucket_name, 'uri': s3_directory + '/' + filename_only + mime_extension}
+r = requests.get(config.viblio_server_url, params=params)
+print r.text
+
