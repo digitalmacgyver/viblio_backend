a933e3052d5a2a5c09da4f6338571033d28b7c59
Matt Hayward <matt@viblio.com>
2013-08-12 15:17:32 -0700

    First draft of test data insertion tool.

--- test/create_test_data.py
+++ test/create_test_data.py
@@ -1,93 +1,64 @@
 #!/usr/bin/python
 
-# 1. Create code to add data.
-# 2. Create doco.
-# 3. Create sample
-
-# Add 15 videos for user.
-# * At least one avi, mp4, mpeg, wmv
-# Videos uploaded through tupsy
-
-# Add 10 faces
-#   * 1 recognized FB
-#   * 1 recognized Viblio user
-#   * 1 named user
-#   * some unidentified users
-# asset_type = image, time_stamp provided.
-
-# meadia_asset_features
-# One for each face, contact ID as appropriate.
-
-# Media comments - One video has no comments, one has one, One has
-# 100, the rest have 2.
-
-# 1. Input us the user uuid.
-# 2. Input is the tuspy parameters.
-# 3. Download videos and faces from staging.
-# 4. Upload through tuspy.
-# 5. Insert additional faces associated with media for filename.
-# 6. 
-
-
-# TODO:
-# 1. Fix "root" as the second position in logging.
-# 2. Fix the "unable to resolve DEBUG" error.
-
-import json
-import uuid
-import hmac
-import os
-
+import logging
+from optparse import OptionParser
 import sys
+
+import boto
+from boto.s3.key import Key
+
+from sqlalchemy import *
+
 sys.path.append("../popeye")
 from appconfig import AppConfig
 config = AppConfig( 'create_test_data' ).config()
 
-from models import *
+from test_utils import get_user_id_for_uuid, create_test_contacts, create_test_videos, upload_file_to_s3, download_file_from_s3
 
+from test_data import faces, videos, contacts
 
-import boto
-import requests
-from boto.s3.key import Key
-
-import logging
 logging.basicConfig( filename = config['logfile'], level = config.loglevel )
 log = logging.getLogger( "create_test_data" )
 
-log.critical( "FOO create_test" )
+verbose = False
 
-from test_utils import upload_file_to_s3, download_file_from_s3
+# Connect to the database.
+log.info( "Creating SQLAlchemy engine for: %s" % ( config.db_name ) )
+engine = create_engine( 'mysql+mysqldb://'
+                        +config.db_user+':'+config.db_pass
+                        +config.db_conn
+                        +config.db_name, echo=verbose )
 
-videos = [
-    'test-01.mov',
-    'test-02.mov',
-    'test-03.mov',
-    'test-04.MOV',
-    'test-05.mov',
-    'test-06.mov',
-    'test-07.mov',
-    'test-08.MTS',
-    'test-09.mp4',
-    'test-0a.mp4',
-    'test-0b.mp4',
-    ]
+user_uuid = None
 
-faces = [
-    'face-01.jpg',
-    'face-02.jpg',
-    'face-03.jpg',
-    'face-04.jpg',
-    'face-05.jpg',
-    'face-06.jpg',
-    'face-07.jpg',
-    'face-08.jpg',
-    'face-09.jpg',
-    'face-0a.jpg',
-    ]
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-u", "--user",
+                  dest="user_uuid",
+                  help="The existing user uuid of the user to add test data for." )
 
-vid_dir = "/wintmp/test_files/"
-face_dir = vid_dir + "faces/"
+    (options, args) = parser.parse_args()
 
+    if not options.user_uuid:
+        parser.print_help()
+        sys.exit(0)
+    else:
+        user_uuid = options.user_uuid
+
+    log.info( "Getting user_id for uuid: %s" % ( user_uuid ) )
+    user_id = get_user_id_for_uuid( engine, user_uuid )
+    log.info( "User id is: %s" % ( user_id ) )
+
+    create_test_contacts( engine, user_id, contacts )
+
+    # DEBUG - today this doesn't put anything in S3, it just formulates
+    # the rows in the database.
+    create_test_videos( engine, user_id, videos, faces, contacts )
+
+
+
+
+'''
 # File downloads from s3
 # DEBUG get all videos
 for video in videos[:1]:
@@ -101,7 +72,7 @@ for face in faces[:1]:
     key = config.test_face_prefix + face
     download_file_from_s3( fname, key )
 
-# Upload video to through tupsy.
+'''
 
 '''
 
@@ -117,41 +88,3 @@ for face in faces:
 
 '''
 
-'''
-
-user = {
-    'email' : 'foo@gmail.com'
-    'id' : 1,
-    'uuid' : 'deadbeef'
-}
-
-taggeduser = {}
-shareduser = {}
-
-# We have 15 videos.
-videos = []
-
-# We have 10 faces.
-contacts = [
-    { 'face' : 'filename.jpg', 'provider': 'local', 'provider_id':None, 
-      'name' : 'name', 'email' : 'email', 'viblio_id' : None },
-    ]
-
-
-
-
-
-def create_test_user():
-    pass
-
-def upload_test_videos():
-    pass
-
-def upload_test_faces():
-    add_face_to_media_assets()
-    add_asset_feature_for_face()
-    add_contact_for_face()
-    pass
-
-
-'''
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,21 +1,223 @@
+import datetime
+import logging
 import os
+import random
 import sys
-sys.path.append("../popeye")
-from appconfig import AppConfig
-config = AppConfig( 'create_test_data' ).config()
-
-from models import *
+import uuid
 
 import boto
 from boto.s3.key import Key
 
-import logging
+from sqlalchemy import *
+
+sys.path.append("../popeye")
+from appconfig import AppConfig
+config = AppConfig( 'create_test_data' ).config()
 
 logging.basicConfig( filename = config['logfile'], level = config.loglevel )
 
 log = logging.getLogger( __name__ )
 
-log.critical( "WHEE - test utils" )
+def _get_conn( engine ):
+    try:
+        if not hasattr( _get_meta, 'conn' ):
+            log.info( "Creating a database connection." )
+            _get_conn.conn = engine.connect()
+        return _get_conn.conn
+    except Exception, e:
+        log.critical( "Failed to get database connection. Error: %s" % e )
+        raise
+
+def _get_meta( engine ):
+    try:
+        log.info( "Getting SQLAlchemy data structures from the database." )
+        if not hasattr( _get_meta, 'meta' ):
+            _get_meta.meta = MetaData()
+            _get_meta.meta.reflect( bind = engine )
+        return _get_meta.meta
+    except Exception, e:
+        log.critical( "Failed to get SQLAlchemy metadata. Error: %s" % e )
+        raise
+
+def get_user_for_uuid( engine, user_uuid ):
+    try:
+        conn = _get_conn( engine )
+        meta = _get_meta( engine )
+        users = meta.tables['users']
+        return conn.execute( select( [users] ).where( users.c.uuid == user_uuid ) ).fetchone()
+    except Exception, e:
+        log.critical( "Failed to find user id for user uuid %s.  Error: %s" % ( user_uuid, e ) )
+        raise
+
+def get_user_id_for_uuid( engine, user_uuid ):
+    try:
+        if not hasattr( get_user_id_for_uuid, 'id_map' ):
+            get_user_id_for_uuid.id_map = {}
+        if user_uuid not in get_user_id_for_uuid.id_map:
+            get_user_id_for_uuid.id_map[user_uuid] = get_user_for_uuid( engine, user_uuid)['id']
+        return get_user_id_for_uuid.id_map[user_uuid]
+    except Exception, e:
+        log.critical( "Failed to find user id for user uuid %s.  Error: %s" % ( user_uuid, e ) )
+        raise
+
+def create_test_contacts( engine, user_id, contact_list ):
+    '''Given a user_id and a data structure of contacts, insert the
+    contacts for that user_id into the database.  Also, populate the
+    contacts data structure with the ids of the inserted contacts.
+
+    contacts = [ { name, email, provider, provider_id, viblio_id }, ... ]'''
+    try:
+        log.info( "Inserting contacts for user_id %s" % ( user_id ) )
+        conn = _get_conn( engine )
+        meta = _get_meta( engine )
+        contacts = meta.tables['contacts']
+        for contact in contact_list:
+            log.info( "Adding contact: %s" % ( contact['name'] ) )
+            result = conn.execute( contacts.insert(),
+                                   id                = None,
+                                   user_id           = user_id,
+                                   contact_name      = contact['name'],
+                                   contact_email     = contact['email'],
+                                   contact_viblio_id = contact['viblio_id'],
+                                   provider          = contact['provider'],
+                                   provider_id       = contact['provider_id'] )
+            contact['id'] = result.inserted_primary_key[0]
+            log.info( "Inserted contact id is: %s" % ( contact['id'] ) )
+    except Exception, e:
+        log.critical( "Failed to insert contacts. Error: %s" % ( e ) )
+        raise
+        
+def create_test_videos( engine, user_id, videos, faces, contacts ):
+    '''Given an array of video, face, and contact data structures
+    populates media, media_assets, media_asset_features with the
+    relevant data.'''
+    try:
+        log.info( "Inserting test videos for user_id %s" % ( user_id ) )
+        conn = _get_conn( engine )
+        meta = _get_meta( engine )
+        media = meta.tables['media']
+        media_assets = meta.tables['media_assets']
+        media_asset_features = meta.tables['media_asset_features']
+        
+        for video in videos:
+            # Add the media row
+            video['uuid'] = str( uuid.uuid4() )
+            v_result = conn.execute( media.insert(),
+                                     id             = None, # Populated by the database.
+                                     user_id        = user_id,
+                                     uuid           = video['uuid'],
+                                     media_type     = 'original',
+                                     title          = 'unit_test_insert',
+                                     filename       = video['s3_key'],
+                                     description    = 'unit_test_insert',
+                                     recording_date = datetime.datetime.now(),
+                                     view_count     = 0,
+                                     lat            = float( "%.8f" % random.uniform(-180,180) ),
+                                     lng            = float( "%.8f" % random.uniform(-180,180) ) )
+            video['id'] = v_result.inserted_primary_key[0]
+
+            # Add the main asset row
+            # DEBUG - assumes this URI exists in S3 already
+            # DEBUG - doesn't set bytes.
+            video['main_uuid'] = str( uuid.uuid4() )
+            result_id = add_asset( conn=conn, media_assets=media_assets, row=video, 
+                                   uuid=video['main_uuid'],
+                                   asset_type='main', 
+                                   mimetype='video/mp4',
+                                   uri=video['uuid']+'/'+video['uuid']+'.mp4',
+                                   metadata_uri=video['uuid']+'/'+video['uuid']+'_metadata.json' )
+            video['main_id'] = result_id
+
+            # Add the thumbnail row
+            # DEBUG - assumes this URI exists in S3 already
+            # DEBUG - doesn't set bytes.
+            video['thumbnail_uuid'] = str( uuid.uuid4() )
+            result_id = add_asset( conn=conn, media_assets=media_assets, row=video, 
+                                   uuid=video['thumbnail_uuid'], 
+                                   asset_type='thumbnail', 
+                                   mimetype='image/jpg',
+                                   uri=video['uuid']+'/'+video['uuid']+'_thumbnail.jpg',
+                                   metadata_uri=None )
+            video['thumbnail_id'] = result_id
+
+            # Add the poster row
+            # DEBUG - assumes this URI exists in S3 already
+            # DEBUG - doesn't set bytes.
+            video['poster_uuid'] = str( uuid.uuid4() )
+            result_id = add_asset( conn=conn, media_assets=media_assets, row=video, 
+                                   uuid=video['poster_uuid'],
+                                   asset_type='poster', 
+                                   mimetype='image/jpg',
+                                   uri=video['uuid']+'/'+video['uuid']+'_poster.jpg',
+                                   metadata_uri=None )
+            video['poster_id'] = result_id
+
+            for face_idx in video['face_idx']:
+                # Add a face row for each face
+                face = faces[face_idx]
+                face_uuid = str( uuid.uuid4() )
+                face_uri = video['uuid']+'/face-' + face_uuid+'.jpg'
+                face_id = add_asset( conn=conn, media_assets=media_assets, row=video, 
+                                     uuid=face_uuid,
+                                     asset_type='face', 
+                                     mimetype='image/jpg',
+                                     uri=face_uri,
+                                     metadata_uri=None, bytes=face['size'] )
+
+                log.info( 'S3 copy of face from %s to %s' % ( face['s3_key'], face_uri  ) ) 
+                bucket = _get_bucket()
+                bucket.copy_key( face_uri, config.bucket_name, face['s3_key'] )
+
+                # Add a feature row for each face
+                contact_id = None
+                if face['contact_idx']:
+                    contact_id = contacts[face['contact_idx']]['id']
+                add_feature( conn=conn, media_asset_features=media_asset_features,
+                             media_asset_id = face_id,
+                             feature_type = 'face',
+                             coordinates = "{ 'x1':0, 'y1':0, 'x2':"+str( face['width'] )+", 'y2':"+str( face['height'] )+" }",
+                             contact_id = contact_id )
+                
+    except Exception, e:
+        log.critical( "Failed to set up test data. Error: %s" % ( e ) )
+        raise
+
+
+def add_feature( conn, media_asset_features, media_asset_id, feature_type, coordinates, contact_id ):
+    try:
+        log.info( "Inserting feature for asset %s of type %s" % ( media_asset_id, feature_type ) )
+        result = conn.execute( media_asset_features.insert(),
+                      id = None,
+                      media_asset_id = media_asset_id,
+                      feature_type = feature_type,
+                      coordinates = coordinates,
+                      contact_id = contact_id )
+        log.info( "Inserted feature has id: %s" % ( result.inserted_primary_key[0] ) )
+    except Exception, e:
+        log.critical( "Failed to insert feature. Error: %s" % ( e ) ) 
+        raise
+
+def add_asset( conn, media_assets, row, uuid, asset_type, mimetype, uri, metadata_uri, bytes=None ):
+    try:
+        log.info( "Inserting asset for %s of type %s" % ( uri, asset_type ) )
+        result = conn.execute( media_assets.insert(),
+                               id             = None, # Populated by the database.
+                               media_id       = row['id'],
+                               uuid           = uuid,
+                               asset_type     = asset_type,
+                               mimetype       = mimetype,
+                               uri            = uri,
+                               location       = 'us',
+                               metadata_uri   = metadata_uri,
+                               view_count     = 0
+                               )
+        log.info( "Inserted asset has id %s" % ( result.inserted_primary_key[0] ) )
+        return result.inserted_primary_key[0]
+    except Exception, e:
+        log.critical( "Failed to insert asset. Error: %s" % ( e ) )
+        raise
+
+
 
 def _get_bucket():
     try:
