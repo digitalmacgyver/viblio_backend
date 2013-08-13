20b2d7d83e9aede645cb60e23d26a2c8c40e8cc4
Matt Hayward <matt@viblio.com>
2013-08-13 13:11:33 -0700

    Schema updates

--- schema/ddl/video_dev_schema_updates.sql
+++
@@ -1,7 +0,0 @@
-use video_dev;
-
-alter table video add uuid varchar(40);
-alter table video add user_id varchar(40);
-alter table video add mimetype varchar( 40);
-alter table video add size int(11) not null default 0;
-alter table video add uri text;
