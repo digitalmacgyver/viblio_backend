599f2cd573afd1a467d243c501605a244807a301
Bidyut Parruck <bparruck@gmail.com>
2013-08-30 20:33:27 -0700

    Updated IntelliVision address, added uid for bidyut@viblio.com

--- popeye/iv_config.py
+++ popeye/iv_config.py
@@ -3,5 +3,7 @@ import pytz
 ## Define parameters
 partner_id = 'VIBLIO'
 local_id = '9876543210'
-iv_host = 'http://71.6.45.227/FDFRRstService/RestService.svc/'
+iv_host = 'http://71.6.45.228/FDFRRstService/RestService.svc/'
 time_zone = pytz.timezone("GMT")
+uid = 'C209A678-03AF-11E3-8D79-41BD85EDDE05'
+
