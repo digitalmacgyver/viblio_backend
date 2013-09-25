f7f0a6686a3c46d582b528a149ae52724c1f2fd0
Bidyut Parruck <bparruck@gmail.com>
2013-09-24 18:42:58 -0700

    Completed 1st pass coding for IntelliVision

--- popeye/video_processing.py
+++ popeye/video_processing.py
@@ -8,30 +8,31 @@ import iv
 import boto
 from boto.s3.key import Key
 
-def get_faces(file_data, log, data):
-    ifile   = file_data['ifile']
-    ofile   = file_data['ofile']
-    s3_key  = file_data['key']
-    uid     = data['info']['uid']
-    uuid    = data['info']['uuid']
+# Our application config
+from appconfig import AppConfig
+try:
+    config = AppConfig( 'popeye' ).config()
+except Exception, e:
+    print( str(e) )
+    sys.exit(1)
 
-#     ## Transcode to AVI for Intellivision
-#     ffopts = ''
-#     cmd = '/usr/local/bin/ffmpeg -v 0 -y -i %s %s %s' % ( ifile, ffopts, ofile )
-#     print( cmd )
-#     if os.system( cmd ) != 0 or not os.path.isfile( ofile ):
-#         raise Exception( 'Failed to generate AVI file: %s' % cmd )
-    ## Copy AVI to S3 and make the URL public
+def get_faces(file_data, log, data):
+    s3_key  = file_data['key']
+    media_uuid = os.path.split(s3_key)[0]
+    uid = iv_config.uid
+    minimum_detection_score = iv_config.minimum_detection_score
+    minimum_recognition_score = iv_config.minimum_recognition_score
+    ## Make S3 URL public for IntelliVision
     try:
-        s3 = boto.connect_s3(iv_config.awsAccess, iv_config.awsSecret)
-        bucket = s3.get_bucket(iv_config.iv_bucket_name)
+        s3 = boto.connect_s3(config.awsAccess, config.awsSecret)
+        bucket = s3.get_bucket(config.bucket_name)
         bucket_contents = Key(bucket)
         bucket_contents.key = s3_key
-        bucket_contents.set_contents_from_filename( ofile )
+        original_acl = bucket_contents.get_acl()
         bucket_contents.make_public()
     except:
-        print 'error copying to S3'   
-    media_url = 'http://s3-us-west-2.amazonaws.com/' + iv_config.iv_bucket_name + '/' + s3_key
+        print 'Error making S3 URL public'   
+    media_url = 'http://s3-us-wes(detection_score > minimum_detection_score)t-2.amazonaws.com/' + config.bucket_name + '/' + s3_key
     print media_url
     session_info = iv.open_session()
     user_id = iv.login(session_info, uid)
@@ -39,29 +40,61 @@ def get_faces(file_data, log, data):
     file_id = response['file_id']
     wait_time = response['wait_time']
     time.sleep(wait_time)
-    tracks = iv.retrieve(session_info, user_id, file_id, uuid)
+    tracks = iv.retrieve(session_info, user_id, file_id, media_uuid)
+    number_of_tracks = int(tracks.numberoftracks.string)
     for i,track in enumerate(tracks.findAll('track')):
-        track_id = i
+        track_id = track.trackid.string
+        formatted_track_id = '%02d' %int(track_id)
+        print formatted_track_id
         person_id = int(track.personid.string)
-        detection_score = int(track.detectionscore.string)
-        if ((detection_score > iv_config.minimum_detection_score) & (person_id < 0)):
-            person_id = iv.add_person(session_info, uid)
-            iv.train_person(session_info, user_id, person_id, track_id, file_id, media_url)
-            track.personid.string = person_id
-    print 'Track number = ' + str(track_id)
-    print 'person_id = ' + str(person_id)
-    print 'detectionscore = ' + track.detectionscore.text
-    print 'Recognition confidence = ' + track.recognitionconfidence.text
-    print track.bestfaceframe.text
+        detection_score = float(track.detectionscore.string)
+        if ( person_id < 0 ):
+            if ( detection_score > minimum_detection_score ):
+                new_person_id = iv.add_person(session_info, user_id)
+                formatted_new_person_id = '%02d' %int(new_person_id)
+                print 'Added a new person: ' + new_person_id
+                print "downloading with best face frame"            
+                url = track.bestfaceframe.string
+                r = requests.get(url)
+                filename = '/mnt/uploaded_files/' + media_uuid + '_face_' + formatted_track_id + '_' + formatted_new_person_id + '.jpg'
+                with open(filename, "wb") as f:
+                    f.write(r.content)
+                face_key = media_uuid + '/' + media_uuid + '_face_' + formatted_track_id + '_' + formatted_new_person_id + '.jpg'
+                print "Uploading face to S3"
+                try:
+                    bucket_contents.key = face_key
+                    bucket_contents.set_contents_from_filename(filename)
+                except:
+                    print 'Upload to S3 failed'
+                try:
+                    iv.train_person(session_info, user_id, new_person_id, track_id, file_id, media_url)
+                    print 'training: ' + str(new_person_id)
+                except:
+                    print 'Failed to train unknown person'
+                # update person_id in tracks to the new person
+                track.personid.string = new_person_id
+                track.bestfaceframe.string = face_key
+            else:
+                print 'TO DO: delete track from the tracks structure and adjust number of tracks'
+                number_of_tracks -= 1
+        else:
+            recognition_score = float(track.recognitionconfidence.string)
+            if ( recognition_score > minimum_recognition_score ):
+                try:
+                    iv.train_person(session_info, user_id, person_id, track_id, file_id, media_url)
+                    print 'training: ' + str(new_person_id)
+                except:
+                    print 'Failed to train known person'
+    tracks.numberoftracks.string = number_of_tracks
     tracks_string = str(tracks)
     tracks_dict = xmltodict.parse(tracks_string)
     tracks_json = json.dumps(tracks_dict)
     fileid_json = "{'file_id': " + file_id + "}"
-    return(fileid_json, tracks_json)
+    # Cleanup permissions, logout & close session
+    bucket_contents.set_acl(original_acl)
     iv.logout(session_info, user_id)
     iv.close_session(session_info)
-    faces_data = {'file_id': file_id}
-    return (faces_data)
+    return(fileid_json, tracks_json)
 
 def transcode_main( file_data, log, data, files=None ):
     ifile = file_data['ifile']
