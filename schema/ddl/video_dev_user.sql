7f58739418ff2c5f216ca1c1c412895a9649aa00
Matt Hayward <matt@viblio.com>
2013-10-28 23:39:05 -0700

    Updated DB credentials

--- schema/ddl/video_dev_user.sql
+++ schema/ddl/video_dev_user.sql
@@ -2,3 +2,13 @@ create user 'video_dev_1' identified by 'video_dev_1';
 grant all on video_dev_1.* to 'video_dev_1';
 revoke drop on video_dev_1.* from 'video_dev_1';
 
+create user 'web_dev' identified by 'Yn8U!2Y52Pt#5MEK';
+grant select, insert, update, delete, create temporary tables, lock tables, execute, trigger on video_dev_1.* to 'web_dev' with max_user_connections 200;
+
+create user 'vwf_dev' identified by '@zjwS3F8CDRm6Bs!';
+grant select, insert, update, delete, create temporary tables, lock tables, execute, trigger on video_dev_1.* to 'vwf_dev' with max_user_connections 200;
+
+create user 'popeye_dev' identified by '&uhq3V7peaBAhExT';
+grant select, insert, update, delete, create temporary tables, lock tables, execute, trigger on video_dev_1.* to 'popeye_dev' with max_user_connections 200;
+
+
--- schema/ddl/video_prod_user.sql
+++ schema/ddl/video_prod_user.sql
@@ -2,3 +2,11 @@ create user 'video_prod' identified by 'video_prod';
 grant all on video_prod.* to 'video_prod';
 revoke drop on video_prod.* from 'video_prod';
 
+create user 'web_prod' identified by 'AVxXwDC9Y%sKaPG@';
+grant select, insert, update, delete, create temporary tables, lock tables, execute, trigger on video_dev.* to 'web_prod' with max_user_connections 200;
+
+create user 'vwf_prod' identified by 'R5j*ApW2pcF*xNsK';
+grant select, insert, update, delete, create temporary tables, lock tables, execute, trigger on video_dev.* to 'vwf_prod' with max_user_connections 200;
+
+create user 'popeye_prod' identified by '393!zyRu@2u&gyT@';
+grant select, insert, update, delete, create temporary tables, lock tables, execute, trigger on video_dev.* to 'popeye_prod' with max_user_connections 200;
