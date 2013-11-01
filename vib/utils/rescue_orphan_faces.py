937dd383eb1e0bec6d12d61339d803973b373f5b
Matt Hayward <matt@viblio.com>
2013-11-01 03:18:01 -0700

    Added face rescuing utility.

--- vib/utils/rescue_orphan_faces.py
+++ vib/utils/rescue_orphan_faces.py
@@ -1,110 +1,122 @@
 #!/usr/bin/env python
 
-import os
-os.environ['BOTO_CONFIG'] = os.path.dirname( __file__ ) + '../config/boto.config'
+import datetime
+import json
+import logging
 from optparse import OptionParser
+from sqlalchemy import and_, func
 import sys
 import time
+import uuid
 
 import vib.config.AppConfig
 config = vib.config.AppConfig.AppConfig( 'viblio' ).config()
 
+import vib.db.orm
+from vib.db.models import *
 
-Wrapper that runs it once every 6 hours
-Supervisor that owns it
+log = logging.getLogger( 'vib.utils.rescue_orphan_faces' )
+log.setLevel( logging.DEBUG )
 
-Logic:
-  Similar to MTurk notify
-  select * from MAF where recognition_result is null and contact_id is null and created_date > 2 days old
-  create new contacts
-  make link
+syslog = logging.handlers.SysLogHandler( address="/dev/log" )
 
+format_string = 'rescue_orphan_faces: { "name" : "%(name)s", "module" : "%(module)s", "lineno" : "%(lineno)s", "funcName" : "%(funcName)s",  "level" : "%(levelname)s", "deployment" : "' + config.VPWSuffix + '", "activity_log" : %(message)s }'
 
-from vib.thirdParty.mturkcore import MechanicalTurk
+sys_formatter = logging.Formatter( format_string )
 
-def get_outstanding_assignments():
-    mt = MechanicalTurk( 
-        { 
-            'use_sandbox'           : True, 
-            'stdout_log'            : True, 
-            'aws_key'     : config.awsAccess,
-            'aws_secret_key' : config.awsSecret
-            }  )
+syslog.setFormatter( sys_formatter )
+syslog.setLevel( logging.INFO )
+consolelog = logging.StreamHandler()
+consolelog.setLevel( logging.DEBUG )
+
+log.addHandler( syslog )
+log.addHandler( consolelog )
+
+def update_orphan_faces( hours=24*3 ):
+    '''Search our database for media_asset_features that are faces,
+    and not part of bad tracks, but which are not associated with a
+    contact.
+
+    Create a new undefined contact for each track with such a face,
+    and assign that face to the contact.'''
     
-    options = { 
-        'Statistic' : 'NumberAssignmentsAvailable', 
-        'TimePeriod' : 'LifeToDate' 
-        }
-
-    result = mt.create_request( 'GetRequesterStatistic', options )
-
-    assignments = mt.get_response_element( 'LongValue', result )
-    
-    if assignments == None:
-        assignments = 0
-    else:
-        assignments = int( assignments )
-
-    print "There are : %s assignments available." % assignments
-    return assignments
-
-def notify_workers( workers, assignments ):
-    ret = True
-
-    mt = MechanicalTurk( 
-        { 
-            'use_sandbox'           : True, 
-            'stdout_log'            : True, 
-            'aws_key'     : config.awsAccess,
-            'aws_secret_key' : config.awsSecret
-            }  )
-    
-    max_workers_per_notification = 99
-    for idx in range( 0, len( workers ), max_workers_per_notification ):
-        worker_list = workers[idx:idx+max_workers_per_notification]
-
-        task_url = 'https://workersandbox.mturk.com/mturk/searchbar?selectedSearchType=hitgroups&searchWords=Viblio&minReward=0.00&x=15&y=17'
-
-        options = { 
-            'Subject' : 'Pending Face Recognition Tasks', 
-            'MessageText' : "There are %d face recognition tasks pending at:\n\n%s\n" % ( assignments, task_url ),
-            'WorkerId' : worker_list
-            }
-
-        result = mt.create_request( 'NotifyWorkers', options )
-        is_valid = 'NotifyWorkersFailureStatus' not in result['NotifyWorkersResponse']['NotifyWorkersResult']
-        if not is_valid:
-            print "Failed to notify some subset of workers: %s" % workers[idx:idx+max_workers_per_notification]
-            ret = False
-
-    return ret
-
-if __name__ == '__main__':
-    # For the beta, we manually maintain a list of workers here.  
-
-    # We can't add a worker to notification until they have done at
-    # least one task for us.
-    workers = [
-        'A3NNF2DM3T5FFQ', # Matt Hayward
-        #'AT57TTIBIJBYY', # Andy Peebles - personal
-        'ANT1E8VYCK300', # Bidyut Parruck
-        'A2UAN9XAD1587R', # video-analytics-inc.com
-        ]
-
-    print "Checking if there are pending notifications."
-    count = 0
     try:
-        count = get_outstanding_assignments()
+        orm = None
+        orm = vib.db.orm.get_session()
+
+        from_when = datetime.datetime.utcnow() - datetime.timedelta( hours=hours )
+        
+        orphan_faces = orm.query( 
+            MediaAssetFeatures.id,
+            MediaAssetFeatures.media_asset_id,
+            MediaAssetFeatures.user_id,
+            MediaAssetFeatures.media_id,
+            MediaAssetFeatures.track_id,
+            MediaAssets.uri,
+            Users.uuid,
+            func.max( MediaAssetFeatures.detection_confidence )
+            ).filter( 
+            and_( 
+                MediaAssets.id == MediaAssetFeatures.media_asset_id,
+                MediaAssets.user_id == Users.id,
+                MediaAssetFeatures.user_id == Users.id,
+                MediaAssetFeatures.contact_id == None, 
+                MediaAssets.asset_type == 'face',
+                MediaAssetFeatures.feature_type == 'face',
+                MediaAssetFeatures.recognition_result != 'bad_track',
+                MediaAssetFeatures.created_date <= from_when
+                ) ).group_by(
+                MediaAssetFeatures.media_id,
+                MediaAssetFeatures.track_id
+                ).all()
+
+        log.debug( json.dumps( {
+                    'message' : "Found %d orphan faces." % len( orphan_faces )
+                    } ) )
+
+        for orphan in orphan_faces:
+            contact = Contacts(
+                uuid        = str( uuid.uuid4() ),
+                user_id     = orphan.user_id,
+                picture_uri = orphan.uri
+                )
+
+            orm.add( contact )
+            log.debug( json.dumps( {
+                        'user_uuid' : orphan.uuid,
+                        'media_asset_feature_id' : orphan.id,
+                        'message' : "Added contact %s for orphan media_id/track_id %s/%s" %  ( contact.uuid, orphan.media_id, orphan.track_id )
+                        } ) )
+
+            faces = orm.query( MediaAssetFeatures ).filter( and_(
+                    MediaAssetFeatures.media_id == orphan.media_id,
+                    MediaAssetFeatures.track_id == orphan.track_id ) ).all()
+            contact.media_asset_features.extend( faces )
+            log.debug( json.dumps( {
+                        'user_uuid' : orphan.uuid,
+                        'message' : "Added %d features to contact %s for orphan media_id/track_id %s/%s" %  ( len( faces ), contact.uuid, orphan.media_id, orphan.track_id )
+                        } ) )
+            
+
+        orm.commit()
+
+        return True
+
     except Exception as e:
-        print "Failed to get assignment count, error was: %s" % e
+        if orm != None:
+            orm.rollback()
+        log.error( json.dumps( {
+                    'message' : "Exception was: %s" % e
+                    } ) )
         raise
-       
+    
+if __name__ == '__main__':
     try:
-        if count > 0:
-            if notify_workers( workers, count ):
-                print "Successfully notified all workers: %s" % workers
-            else:
-                print "Failed to notify some workers."
+        while update_orphan_faces():
+            # Run once every 6 hours
+            time.sleep( 6*60*60 )
     except Exception as e:
-        print "Failed to notify workers, error was: %s" % e
+        log.error( json.dumps( {
+                    'message' : "Exception was: %s" % e
+                    } ) )
         raise
