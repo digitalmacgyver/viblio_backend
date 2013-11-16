b8900f7d1b9127708f48c20f126909536234e070
Matt Hayward <matt@viblio.com>
2013-11-15 22:43:06 -0800

    Adding restart scripts for local development.

---
+++ restart-backend-popeye-vib.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo /etc/init.d/popeye stop
+sudo /etc/init.d/supervisor stop
+sudo make LVL=local deploy_popeye deploy_utils deploy_vib
+sudo /etc/init.d/popeye start
+sudo /etc/init.d/supervisor start
