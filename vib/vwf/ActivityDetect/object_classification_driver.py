6e6ebe5711725fb3566f1fbd893ecc320198d4b5
Matt Hayward <matt@viblio.com>
2014-02-15 02:14:40 -0800

    Bug fixes

--- vib/vwf/ActivityDetect/object_classification_driver.py
+++ vib/vwf/ActivityDetect/object_classification_driver.py
@@ -6,6 +6,6 @@ def get_confidence(video_file,working_dir,model_dir):
     python_path = os.path.dirname( __file__ ) + '/../../../object_classification/classification/'
     os.system("cd %s; PYTHONPATH=$PYTHONPATH:%s ./video_classifier.sh %s %s %s" %(path,python_path,video_file,working_dir,model_dir) )
     with open(working_dir+'/result.txt') as f:
-        confidence=float(f.readlines[0])
+        confidence=float( f.readlines()[0] )
     print 'confidence value ',confidence
     return confidence
